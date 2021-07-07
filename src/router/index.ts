f65ba97ce90c1e8f3bc859599986f91371288907
Akiff Manji <akiff.manji@gmail.com>
2021-07-07 08:10:10 -0700

    Project scaffolding
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

---
+++ src/router/index.ts
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "@/views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "@/views/About.vue"),
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: () =>
+      import(/* webpackChunkName: "contact" */ "@/views/Contact.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Home goes here!</h1>
+</template>
