f978cdfa58d1b525dd0ec4b118d4d71567acf127
wadeking98 <wkingnumber2@gmail.com>
2021-07-22 03:25:29 -0700

    added styling to search bar

--- src/components/search/SearchComponent.vue
+++ src/components/search/SearchComponent.vue
@@ -1,95 +1,87 @@
 <template>
-<div>
-    <v-row>
-      <v-col>
-        <v-toolbar-title>Find an organization</v-toolbar-title>
-      </v-col>
+  <div>
+      <p class="font-weight-bold mb-1" :hidden="$vuetify.breakpoint.mdAndDown" >Find an organization</p>
       
-    </v-row>
-    <v-row>
-      <v-col><v-autocomplete
-      v-model="select"
-      :loading="loading"
-      :items="items"
-      :search-input.sync="search"
-      cache-items
-      class="mx-4"
-      flat
-      hide-no-data
-      hide-details
-      label=""
-      solo-inverted
+        <v-autocomplete
+        v-model="select"
+        :loading="loading"
+        :items="items"
+        :search-input.sync="search"
+        cache-items
+        flat
+        hide-no-data
+        hide-details
+        label=""
+        solo-inverted
+        append-outer-icon="mdi-magnify"
+        @click:append-outer="sendSearchReq"
+      >
+      </v-autocomplete>
       
-    ></v-autocomplete>
-     <v-icon>mdi-magnify</v-icon>
-    </v-col>
-   
-    </v-row>
-</div>
-    
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, PropSync, Vue, Watch } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import { mapActions, mapGetters } from "vuex";
-import _ from 'lodash-es';
+import _ from "lodash-es";
 import { HttpResponse } from "@/services/http.service";
 import { IApiPagedResult } from "@/interfaces/api/result.interface";
 import { ISearchAutocomplete } from "@/interfaces/api/v3/search-autocomplete.interface";
-
-
 
 @Component({
   computed: {
     ...mapGetters(["loading"]),
   },
   methods: {
-    ...mapActions(["getAutocomplete","setLoading"]),
+    ...mapActions(["getAutocomplete", "setLoading"]),
   },
 })
-
 export default class SearchComponent extends Vue {
+  select!: string;
+  search!: string;
+  items!: Array<string>;
+  states!: Array<string>;
+  setLoading!: (loading: boolean) => void;
+  debounceSearchReq = _.debounce(this.searchReq, 500);
+  getAutocomplete!: (
+    query: string
+  ) => Promise<HttpResponse<IApiPagedResult<ISearchAutocomplete>>>;
 
-    select!:string
-    items!:Array<string>
-    states!:Array<string>
-    setLoading!: (loading: boolean) => void;
-    debounceSearchReq = _.debounce(this.searchReq, 500)
-    getAutocomplete!: (query:string) => Promise<HttpResponse<IApiPagedResult<ISearchAutocomplete>>>;
-
-    data() {
+  data() {
     return {
-        items: [],
-        search: null,
-        select: null,
-      }
-    
-    }
-    get something() : string{
-        return this.select;
-    }
-
-    @Watch('search')
-  onChildChanged(val: string, oldVal: string) {
-    this.debounceSearchReq(val)
+      items: [],
+      search: null,
+      select: null,
+    };
+  }
+  get something(): string {
+    return this.select;
   }
 
-    searchReq (val: string) {
-        val && val !== this.select && this.querySelections(val)
-    }
+  @Watch("search")
+  onChildChanged(val: string, oldVal: string) {
+    this.debounceSearchReq(val);
+  }
 
-    querySelections (v:string) {
-      this.setLoading(true)
-      //Simulated ajax query
-      this.getAutocomplete(v).then(resp =>{
-        const autocompleteResults=resp.data.results.map(item=>item.value)
-        this.items = autocompleteResults.filter(e => {
-          return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
-        })
-        this.setLoading(false)
-      })
-    }
-    
-      
+  searchReq(val: string) {
+    val && val !== this.select && this.querySelections(val);
+  }
+
+  querySelections(v: string) {
+    this.setLoading(true);
+    //Simulated ajax query
+    this.getAutocomplete(v).then((resp) => {
+      const autocompleteResults = resp.data.results.map((item) => item.value);
+      this.items = autocompleteResults.filter((e) => {
+        return (e || "").toLowerCase().indexOf((v || "").toLowerCase()) > -1;
+      });
+      this.setLoading(false);
+    });
+  }
+
+  sendSearchReq(){
+    console.log(this.search)
+  }
 }
 </script>
