69dadb05ca479077a81435cbb7cc746ec741bb60
wadeking98 <wkingnumber2@gmail.com>
2021-07-22 20:06:26 -0700

    working on tab links

--- src/components/entity/EntityResult.vue
+++ src/components/entity/EntityResult.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
+    <v-icon>mdi-arrow-left</v-icon><a  href="/" append-icon="mdi-map-marker">Back to search</a>
     <div>Entity Result goes here</div>
+    <v-tabs v-model="currentTab">
+      <v-tab v-for="(item,i) in tabItems" :key="i" @click="tabClick(item.refname)">{{item.text}}</v-tab>
+    </v-tabs>
     <div v-if="selectedTopic">{{ selectedTopic }}</div>
     <div v-if="selectedTopicCredentialSet">
       {{ selectedTopicCredentialSet }}
+    </div>
+    <div id="ItemOne">
+      #ItemOne.
     </div>
   </div>
 </template>
@@ -12,6 +19,7 @@
 import { ITopic } from "@/interfaces/api/v2/topic.interface";
 import { Component, Vue } from "vue-property-decorator";
 import { mapActions, mapGetters } from "vuex";
+import router from "@/router/index"
 
 @Component({
   computed: {
@@ -27,6 +35,7 @@ import { mapActions, mapGetters } from "vuex";
 })
 export default class EntityResult extends Vue {
   setLoading!: (loading: boolean) => void;
+  currentTab!: string;
   fetchFormattedIdentifiedTopic!: ({
     sourceId,
     type,
@@ -35,6 +44,21 @@ export default class EntityResult extends Vue {
     type: string;
   }) => Promise<void>;
   fetchTopicCredentialSet!: (id: number) => Promise<void>;
+
+  data (){
+    return{
+      currentTab:null,
+      tabItems:[
+        {text:"Item One", refname:"ItemOne"},
+        {text:"Item Two", refname:"ItemTwo"},
+        {text:"Item Three", refname:"ItemThree"}
+      ]
+    }
+  }
+
+  tabClick(refname:string){
+    this.$vuetify.goTo(refname, {duration:500, easing:'easeInOutCubic'})
+  }
 
   async created(): Promise<void> {
     this.setLoading(true);
