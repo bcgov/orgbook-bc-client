8d7f2951545b24ffa74280a1587d6b2ac3a8314c
Akiff Manji <akiff.manji@gmail.com>
2021-08-13 12:27:29 -0700

    Adds i18n
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "i18n",
+      enableInSFC: true,
+    },
+  },
 };
