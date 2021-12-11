dcbed5c6c81ad38d6817c9f88e0bb654222dc620
wadeking98 <wkingnumber2@gmail.com>
2021-12-10 16:53:40 -0800

    added favicons and orgbook title
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = "Orgbook BC";
+        return args;
+      })
+  },
   transpileDependencies: ["vuetify"],
   pluginOptions: {
     i18n: {
