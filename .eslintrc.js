0d1354129c9d1831d523c4dbdeacd177caffbe0e
Akiff Manji <amanji@petridish.dev>
2024-04-27 21:49:46 +0000

    chore: update eslint packages
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     "@vue/prettier/@typescript-eslint",
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2022,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
