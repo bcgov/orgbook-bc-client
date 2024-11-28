0be240ccbeb43d8da18eaf3e298a54c161d1b0f6
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2024-11-28 11:51:51 -0800

    Fixed error where i18n translations kept failing
    Signed-off-by: Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,21 +3,30 @@ import VueI18n, { LocaleMessages } from "vue-i18n";
 
 Vue.use(VueI18n);
 
-function loadLocaleMessages(): LocaleMessages {
-  const locales = import.meta.glob("./[A-Za-z0-9-_,\s]+\.json");
+// Define the shape of your JSON files
+interface LocaleData {
+  [key: string]: string;
+}
+
+async function loadLocaleMessages(): Promise<LocaleMessages> {
+  const locales: Record<string, () => Promise<LocaleData>> = import.meta.glob(
+    "./[A-Za-z0-9-_,s]+.json"
+  );
   const messages: LocaleMessages = {};
   for (const key in locales) {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
     if (matched && matched.length > 1) {
       const locale = matched[1];
-      messages[locale] = locales[key];
+      const x = locales[key];
+      messages[locale] = await locales[key]();
     }
   }
+  console.log("local messages are", messages);
   return messages;
 }
 
 export default new VueI18n({
   locale: import.meta.env.VUE_APP_I18N_LOCALE || "en",
   fallbackLocale: import.meta.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
-  messages: loadLocaleMessages(),
+  messages: await loadLocaleMessages(),
 });
